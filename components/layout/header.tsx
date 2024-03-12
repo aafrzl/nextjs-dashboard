d9211ed93d71899b09afa703bfd278bf7fca93f2
Afrizal Mufriz Fouji <63228544+aafrzl@users.noreply.github.com>
2024-03-12 16:27:39 +0700

    update readme & getSession data

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -3,8 +3,12 @@ import { cn } from "@/lib/utils";
 import { MobileSidebar } from "./mobile-sidebar";
 import { UserNav } from "./user-nav";
 import Link from "next/link";
+import getUserSession from "@/lib/get-session";
+import { User } from "@supabase/supabase-js";
 
-export default function Header() {
+export default async function Header() {
+  const { data } = await getUserSession();
+
   return (
     <div className="fixed top-0 left-0 right-0 supports-backdrop-blur:bg-background/60 border-b bg-background/95 backdrop-blur z-20">
       <nav className="h-14 flex items-center justify-between px-4">
@@ -32,7 +36,7 @@ export default function Header() {
         </div>
 
         <div className="flex items-center gap-2">
-          <UserNav />
+          <UserNav user={data.session?.user as User} />
           <ThemeToggle />
         </div>
       </nav>
--- components/layout/user-nav.tsx
+++ components/layout/user-nav.tsx
@@ -11,25 +11,40 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-export function UserNav() {
+import { User } from "@supabase/supabase-js";
+
+interface Props {
+  user: User | null;
+}
+
+export function UserNav({ user }: Props) {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button variant="ghost" className="relative h-8 w-8 rounded-full">
           <Avatar className="h-8 w-8">
             <AvatarImage
-              src={"https://avatars.dicebear.com/api/avataaars/1.svg"}
-              alt={"avatar"}
+              src={
+                user?.user_metadata.avatar_url ||
+                "https://www.gravatar.com/avatar/"
+              }
+              alt={user?.user_metadata.full_name || "Unknown"}
             />
-            <AvatarFallback>{"CN"}</AvatarFallback>
+            <AvatarFallback>
+              {user?.user_metadata.full_name?.charAt(0) || "U"}
+            </AvatarFallback>
           </Avatar>
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent className="w-56" align="end" forceMount>
         <DropdownMenuLabel className="font-normal">
           <div className="flex flex-col space-y-1">
-            <p className="text-sm font-medium leading-none">Name</p>
-            <p className="text-xs leading-none text-muted-foreground">Email</p>
+            <p className="text-sm font-medium leading-none">
+              {user?.user_metadata.full_name || "Unknown"}
+            </p>
+            <p className="text-xs leading-none text-muted-foreground">
+              {user?.email}
+            </p>
           </div>
         </DropdownMenuLabel>
         <DropdownMenuSeparator />
